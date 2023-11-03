0213a2afe9049d7405a3f030d0612e8091d50eac
sx <si.xiang2@iwhalecloud.com>
2023-11-03 11:15:33 +0800

    增加测试启动类

---
+++ application-start-demo/src/main/java/com/sxxxxx/application/demo/ApplicationStartDemo.java
@@ -0,0 +1,13 @@
+package com.sxxxxx.application.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStartDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartDemo.class, args);
+    }
+
+}
